e749f6d80b6b8ab4ab103ce18a6f8d7f0a91970a
Mick <mickster.gallagher@gmail.com>
2015-11-26 08:51:40 -0800

    Removing unnecessary save/post action specificer from api resource

--- grails-app/controllers/UrlMappings.groovy
+++ grails-app/controllers/UrlMappings.groovy
@@ -3,9 +3,7 @@ class UrlMappings {
     static mappings = {
 
         // Implements the get/put/post/delete scaffolding for the TodoRestController
-        "/api/todos" (resources: "todoRest") {
-            action = [POST: "save"]
-        }
+        "/api/todos" (resources: "todoRest")
 
         "/$controller/$action?/$id?(.$format)?"{
             constraints {
