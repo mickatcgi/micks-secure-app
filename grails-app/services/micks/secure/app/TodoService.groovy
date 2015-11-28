aa10595549a2767681f7b60d76608a9425eed8de
Mick <mickster.gallagher@gmail.com>
2015-11-27 16:35:07 -0800

    Adding mail service but not getting it to work

--- grails-app/services/micks/secure/app/TodoService.groovy
+++ grails-app/services/micks/secure/app/TodoService.groovy
@@ -10,6 +10,16 @@ class TodoException extends RuntimeException {
 @Transactional
 class TodoService {
 
+    def mailService
+
+    public TodoService() {
+        // Constructor only exists because if it isn't here the mailService goes null after one
+        // or two tries and is never accessible. This constructor makes sure it never goes null.
+        // There must be some bug with service injection - unable to find an answer on Google.
+        log.info("=====================================================================")
+        log.info("Constructor of TodoService...initializing")
+    }
+
     public Todo saveTodo(Todo todo) {
 
         log.info("In saveTodo() : ${todo?.description}")
@@ -28,11 +38,30 @@ class TodoService {
                 log.info("NEW: TodoService validated successfully: ${todo?.description}")
             }
             savedTodo = todo.save(failOnError: true, flush: true)
+            //welcomeTextEmail()
         } else {
             log.info("TodoService validation failed for: ${todo?.description}")
             throw new TodoException(message: "Todo failed validation", todo: todo)
         }
 
         return savedTodo
+    }
+
+    def welcomeTextEmail() {
+        String recipient = "mickster.gallagher@gmail.com"
+        log.info("Sending a TEXT email now to '${recipient}'")
+        try {
+            mailService.sendMail {
+                to recipient
+                subject "Welcome to Hell...er, I mean Grails"
+                text """
+                Hi, ${recipient}. Great to have you on board
+                The devil..er, I mean the Grails team.
+                """
+            }
+        } catch (Exception e) {
+            log.error("Sending email failed with error: ${e.getMessage()} at ${new Date()}")
+        }
+        return
     }
 }
