bdfba2b84b1cb776f6f42bd46d63c0b3b1a1d3d7
Mick <mickster.gallagher@gmail.com>
2015-11-30 14:17:45 -0800

    Adding some formatting to todo list

--- grails-app/assets/javascripts/singlePageController.js
+++ grails-app/assets/javascripts/singlePageController.js
@@ -4,13 +4,20 @@ angular.module('micks-todos', ['restangular'])
 })
 .controller("singlePageController",
     ["$scope", "Restangular", "$log",
+
         function ($scope, Restangular, $log) {
 
             $scope.name = "singlePageController";
             $scope.allTodos = []
-            Restangular.all('todes').getList().then(function(result) {
-                $scope.allTodos = result
-            })
+            Restangular.all('todes').getList().then(
+                function(result) {
+                    $scope.allTodos = result
+                    $log.debug("Name = " + $scope.name + " AllTodos length = "
+                        + $scope.allTodos.length)
+                },
+                function(error) {
+                    $log.error("Error in REST calll -> " + error.data.errorMessage)
+                }
+            )
 
-            $log.debug("Name = " + $scope.name + " AllTodos length = " + $scope.allTodos.length)
 }]);
--- grails-app/views/todeRest/singlePage.gsp
+++ grails-app/views/todeRest/singlePage.gsp
@@ -10,12 +10,14 @@
             <h1>Mick's SPA displaying single page Todo</h1>
             <f:display bean="todo"/>
 
-            <div id="list-todo" ng-controller="singlePageController" ng-cloak>
+            <div class="property-list" ng-controller="singlePageController" ng-cloak>
+
                 <h1>Mick's SPA Restangular Pulling ALL Todo from {{name}}</h1>
                 <h2>Found {{allTodos.length}} todos</h2>
                 <ul>
-                    <li ng-repeat="myTodo in allTodos">Todo = {{myTodo.description}}</li>
+                    <li class="fieldcontain" ng-repeat="myTodo in allTodos">Todo = {{myTodo.description}}</li>
                 </ul>
+
             </div>
         </div>
 
